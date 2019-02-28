d05ff167aa605a6f9cecb6114bb7d4c5fe5f33bd
yimengnan <572828907@qq.com>
2019-02-28 18:23:13 +0800

    Test修改，提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+/**
+* @Description:    测试类
+* @Author:         yimengnan
+* @CreateDate:     2019/2/28 18:19
+*/
+public class Test{
+    /**
+     * @param null
+    * @return  void
+    * @date        2019/2/28 18:21
+    */
+    public static void main(String[] args){
+        System.out.println("你干嘛呢");
+    }
+}
